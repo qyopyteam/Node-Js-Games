e104b1f75fdcb5d89e28818ed960c8277fb3c239
qyopyteam <143008995+qyopyteam@users.noreply.github.com>
2023-08-23 16:53:39 +0200

    Create Rock, Paper, Scissors.js
    Create Rock, Paper, Scissors.js

---
+++ Rock, Paper, Scissors.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function rockPaperScissors() {
+  const choices = ["rock", "paper", "scissors"];
+
+  rl.question("Choose rock, paper, or scissors: ", (userChoice) => {
+    userChoice = userChoice.toLowerCase();
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(`Computer chose: ${computerChoice}`);
+
+    if (!choices.includes(userChoice)) {
+      console.log("Invalid choice. Please choose rock, paper, or scissors.");
+    } else if (userChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("You win!");
+    } else {
+      console.log("Computer wins!");
+    }
+
+    rl.close();
+  });
+}
+
+rockPaperScissors();
